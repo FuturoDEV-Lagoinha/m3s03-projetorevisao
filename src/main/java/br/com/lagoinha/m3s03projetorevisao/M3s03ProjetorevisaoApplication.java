4cb2da94e7c2a4454a396fd3051e4274d0512af3
Gabriel Augustin <gabriel.augustin@edu.sc.senai.br>
2023-05-16 20:04:48 -0300

    first commit

---
+++ src/main/java/br/com/lagoinha/m3s03projetorevisao/M3s03ProjetorevisaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.lagoinha.m3s03projetorevisao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M3s03ProjetorevisaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M3s03ProjetorevisaoApplication.class, args);
+    }
+
+}
